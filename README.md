6f64ca40a97d4485abd34a9203c4fccc52cd3e9c
José Medina <jmedina@elplazas.com>
2021-04-05 00:34:36 -0400

    proyect configured

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # API CHINCHIN
+
+## Proyecto desarrollado en ExpressJs. Requiere de [node JS](https://nodejs.org/es/download/) para poder iniciar
+
+## Siga los siguientes pasos para iniciar el proyecto
+
+1. Instale las dependencias con el comando:
+
+>`npm i`
+
+ó en caso de tener instalado [yarn](https://yarnpkg.com/):
+
+>`yarn`
+
+2. Inicie el servidor con el comando:
+
+>`node server`
+
+## Hay dos formas de consumir la API:
+
+1. A través del link <http://localhost:3000> se retornará un JSON con el equivalente de la unidad de cada moneda (BTC, ETH, DASH, EUR, PTR y VEF) en USD.
+2. A través del link <http://localhost:3000?usd=amount> (donde **amount** debe ser reemplazado por un número mayor a 0)
--- server.js
+++ server.js
@@ -1,15 +1,122 @@
-import express from 'express'
+import express, { response } from 'express'
+import env from 'dotenv'
+import axios from 'axios'
+
+env.config()
 
 const app = express()
 
-const port = 3000
+const optionsExchange = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 20,
+  style: 'currency',
+  currency: 'USD'
+}
 
-app.get('/', (req, res) => {
-  return res.send({
-    message: 'Received a GET HTTP method'
-  })
+const optionsCurrency = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 20
+}
+
+app.get('/', async (req, res) => {
+  try {
+    const { usd } = req.query
+    if (isNaN(usd)) throw new Error('Debe pasar un número como parámetro')
+    if (usd <= 0) throw new Error('El número debe ser mayor a 0')
+
+    const exchange = []
+    let vef = +process.env.VEF
+    let ptr = +process.env.PTR
+
+    const [response1, response2] = await Promise.all([
+      axios.get('/cryptocurrency/listings/latest', {
+        baseURL: 'https://pro-api.coinmarketcap.com/v1',
+        headers: {
+          'X-CMC_PRO_API_KEY': process.env.APIKEY,
+          'Content-Type': 'application/json'
+        },
+        data: {
+          'start': '1',
+          'limit': '5000',
+          'convert': 'USD'
+        }
+      }),
+      axios.get(`/latest?access_key=${process.env.EXCHANGERATEAPIKEY}`, {
+        baseURL: 'http://api.exchangeratesapi.io/v1'
+      })
+    ])
+
+    const cryptocurrencies = response1.data.data.filter(currency => !!currency.symbol.match(/^BTC$|^ETH$|^DASH$/g))
+    cryptocurrencies.forEach(currency => {
+      if (!!usd)
+        exchange.push({
+          currency: currency.name,
+          [`exchangeTo${currency.symbol}`]: +usd / currency.quote.USD.price,
+          formated: (+usd / currency.quote.USD.price).toLocaleString('es-ES', optionsCurrency)
+        })
+      else exchange.push({
+        currency: currency.name,
+        priceInUSD: currency.quote.USD.price,
+        formated: (currency.quote.USD.price).toLocaleString('es-ES', optionsExchange)
+      })
+    })
+
+    const eur = response2.data.rates.USD
+
+    if (!!usd) {
+      exchange.push({
+        currency: 'EURO',
+        exchangeToEUR: +usd / eur,
+        formated: (+usd / eur).toLocaleString('es-ES', optionsCurrency)
+      })
+    } else {
+      exchange.push({
+        currency: 'EURO',
+        priceInUSD: eur,
+        formated: (eur).toLocaleString('es-ES', optionsExchange)
+      })
+    }
+
+    try {
+      const response3 = await axios.post(
+        '/price',
+        {
+          coins: ['USD', 'PTR'],
+          fiats: ['Bs']
+        },
+        {
+          baseURL: 'https://petroapp-price.petro.gob.ve',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+      vef = response3.data.data.USD.BS
+      ptr = response3.data.data.PTR.BS / vef
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    exchange.push({
+      currency: 'Petros',
+      [!!usd ? 'exchangeToPTR' : 'priceInUSD']: !!usd ? +usd * ptr : 1 / ptr,
+      formated: (!!usd ? +usd * ptr : 1 / ptr).toLocaleString('es-ES', optionsCurrency)
+    })
+    exchange.push({
+      currency: 'Bolívares Venezolanos',
+      [!!usd ? 'exchangeToVEF' : 'priceInUSD']: !!usd ? +usd * vef : 1 / vef,
+      formated: (!!usd ? +usd * vef : 1 / vef).toLocaleString('es-ES', optionsCurrency)
+    })
+
+    return res.send(exchange)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({
+      error: err.message
+    })
+  }
 })
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
 )
